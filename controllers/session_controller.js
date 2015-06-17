dd8f632d08c0605131284fdb3b4960ebe84a6931
José Ramón Jiménez Reyes <jrjimenezr@gmail.com>
2015-06-17 12:03:30 +0200

    Autorización

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,3 +1,13 @@
+// MW de autorización de accesos HTTP restringidos
+exports.loginRequired = function(req, res, next) {
+  if (req.session.user) {
+    next();
+  }
+  else {
+    res.redirect('/login');
+  }
+}
+
 //GET /login
 exports.new = function(req, res) {
   var errors = req.session.errors || {};
@@ -13,7 +23,7 @@ exports.create = function(req, res) {
   var userController = require('./user_controller');
   userController.autenticar(login, password, function(error, user) {
     if (error) {
-      req.session.errors = [{"message": "Se ha producido un error: " + error}];
+      req.session.errors = {"message": "" + error};
       res.redirect("/login");
       return;
     }
